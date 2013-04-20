a37ef278befe34019e0609427c4b251e50ac4d13
specc <specc@specc-ThinkPad-X61-Tablet.(none)>
2013-04-20 11:18:32 -0700

    Simplified the design

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 /*
 Required Libraries 
 */
-var _ = require('underscore')._;
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
-var async = require('async');
 var fs = require('fs'); // file system 
 var email = require('emailjs');
 var cronJob = require('cron').CronJob;
@@ -20,21 +18,22 @@ var server = email.server.connect({
 });
 
 var WebScraper = (function() {
+
   var article = {
     context: "",
     name: "no name",
     links: [],
     date: ""
   };   
+
   var sendEmail = function() {
-    console.log(article.context);
     fs.writeFile('./'+article.name,article.context,function(err) {
       if (err) return console.log(err);
 	    server.send({
 			  text: "attached articles :)",
 			  from: config.email.from,
 			  to: config.email.to, 
-			  subject: "Scarper Result",
+			  subject: "Scraper Result",
 			  attachment:{path:"./"+article.name,type:"text/plain",name:article.name}
 		  },function(err, message) { 
 			  console.log(err || message);
@@ -42,55 +41,52 @@ var WebScraper = (function() {
 	  });
 	  article.date = new Date().toString();	
   };			
+
   var fetchTargets = function() {
+
+    // clear out cache 
+    article.context = "";
+    article.links = [];
+
    	var todayDate = new Date();
 		var fileName = todayDate.getMonth()+1 +"-"+todayDate.getDate()+".txt";
 		article.name = fileName;
-    
-    // fetch links   
+    console.log(fileName);    
+
     request({ uri: 'http://mainichi.jp/feature/maisho/' }, function (error, response, body) {
       if (error && response.statusCode !== 200) {
         console.log('Error when trying to contact target website')
       }   
       $ = cheerio.load(body);
-      async.series([
-      function(callback){
-        $(".MaiLink").first().find("li a").each(function(index,element){
-          article.links.push($(this).attr("href"));
+      // fetch link and article content
+      var $links = $(".MaiLink").first().find("li a");
+      var numOfLinks =  $links.length;
+      $links.each(function(index,element){
+        var lastLink = ((numOfLinks - 1) == index)? true : false;            
+        var linkAddr = $(this).attr("href");            
+        article.links.push(linkAddr); 
+        request({ uri: linkAddr }, function (error, response, body) {
+          if (error && response.statusCode !== 200) {
+            console.log('Error when contacting target');
+          }
+          $ = cheerio.load(body);
+          article.context = article.context + "===================================================== \r \n" +  $("h1.NewsTitle").text() + '\r\n' +$("p.Credit").text() + '\r\n' + $("div.NewsBody").text() + '\r\n';
+          if(lastLink === true){
+            WebScraper.sendEmail();                
+          }
         });
-        callback(null,"done");                  
-      },
-      function(callback) {
-        var orgCallback = callback;         
-        async.eachSeries(article.links,function(item,callback){
-          // fetch context of the list of links 
-          request({ uri: item }, function (error, response, body) {
-            if (error && response.statusCode !== 200) {
-              console.log('Error when contacting target');
-            }
-            $ = cheerio.load(body);
-            article.context = article.context + "===================================================== \r \n" +  $("h1.NewsTitle").text() + '\r\n' +$("p.Credit").text() + '\r\n' + $("div.NewsBody").text() + '\r\n';
-              callback();
-          });
-        },function() {
-          sendEmail();
-          orgCallback(null,"done");
-        });
-      }
-      ],function(err,results) {
-        //call back         
-        return "done"
       });
     });
   };   
   return {
     article:article,
-    fetch:fetchTargets
+    fetch:fetchTargets,
+    sendEmail: sendEmail
   };
 
 })();
 
-var job = new cronJob('0 45 21 * * *', function(){
+var job = new cronJob('0 16 18 * * *', function(){
   WebScraper.fetch()
 });
 
@@ -99,6 +95,13 @@ job.start();
 app.get('/status', function(req, res) {
 	res.send(WebScraper.article);
 });
+
+app.get('/trigger', function(req, res) {
+  WebScraper.fetch();
+	res.send(WebScraper.article);
+});
+
+
 app.listen(3000);
 
 
